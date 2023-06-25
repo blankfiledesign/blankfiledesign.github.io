2605a859b01361266b74e3b27d42743aa1361d0c
moondesignweb <moondesignweb@gmail.com>
2023-06-25 09:47:42 -0300

    cambio completo de diseño

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let animado = document.querySelectorAll(".animado");
+let articulo = document.querySelectorAll(".articulo");
 
 function mostrarScroll(){
 
    let scrollTop = document.documentElement.scrollTop;
 
-   for (var i=0; i < animado.length; i++ ) {
-    let alturaAnimado = animado[i].offsetTop;
+   for (var i=0; i < articulo.length; i++ ) {
+    let alturaAnimado = articulo[i].offsetTop;
 
     if( alturaAnimado - 900 < scrollTop ){
-        animado[i].style.opacity = 1;
+        articulo[i].style.opacity = 1;
 
 
-        animado[i].classList.add("mostrarIzquierda")
+        articulo[i].classList.add("mostrarIzquierda")
     }
    }
 }
@@ -22,21 +22,43 @@ window.addEventListener('scroll', mostrarScroll);
 
 /*derecha a izquierda*/
 
-let animadoD = document.querySelectorAll(".animadoD");
+let articulo2 = document.querySelectorAll(".articulo2");
 
 function mostrarScrollD(){
 
    let scrollTop = document.documentElement.scrollTop;
 
-   for (var i=0; i < animadoD.length; i++ ) {
-    let alturaAnimadoD = animadoD[i].offsetTop;
+   for (var i=0; i < articulo2.length; i++ ) {
+    let alturaAnimadoD = articulo2[i].offsetTop;
 
     if( alturaAnimadoD - 900 < scrollTop ){
-        animadoD[i].style.opacity = 1;
+        articulo2[i].style.opacity = 1;
 
-        animadoD[i].classList.add("mostrarDerecha")
+        articulo2[i].classList.add("mostrarDerecha")
     }
    }
 }
 
 window.addEventListener('scroll', mostrarScrollD);
+
+
+/*abajo a arriba*/
+
+let articulo1 = document.querySelectorAll(".articulo1");
+
+function mostrarScrollA(){
+
+   let scrollTop = document.documentElement.scrollTop;
+
+   for (var i=0; i < articulo1.length; i++ ) {
+    let alturaAnimadoA = articulo1[i].offsetTop;
+
+    if( alturaAnimadoA - 900 < scrollTop ){
+        articulo1[i].style.opacity = 1;
+
+        articulo1[i].classList.add("mostrarAbajo")
+    }
+   }
+}
+
+window.addEventListener('scroll', mostrarScrollA);
