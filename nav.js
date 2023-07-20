3d617c4851af930d0851f322bbab9706f4a4657d
moondesignweb <moondesignweb@gmail.com>
2023-07-20 11:56:52 -0300

    efecto al header al scroll

---
+++ nav.js
@@ -0,0 +1,17 @@
+const div = document.querySelector('header');
+let prevY = window.scrollY;
+
+window.addEventListener('scroll', function( ){
+    if(prevY > window.scrollY){
+        header.classList.remove('off')
+    }else{
+        header.classList.add('off')
+    }
+
+    if(window.scrollY > 270){
+        header.classList.add('solid');
+    }else{
+        header.classList.remove('solid')
+    }
+    prevY = window.scrollY;
+});
