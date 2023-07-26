458e88658e28495d28ec8b9ef30261647e55be2c
moondesignweb <moondesignweb@gmail.com>
2023-07-26 18:05:21 -0300

    btn - read more

---
+++ js/readmore.js
@@ -0,0 +1,53 @@
+
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show');
+
+    if(hideText.classList.contains('show')){
+        hideText_btn.innerHTML = 'Leer menos'
+    }
+    else {
+        hideText_btn.innerHTML = 'Leer mas'
+    }
+}
+
+
+let hideText_btn2 = document.getElementById('hideText_btn2');
+
+let hideText2 = document.getElementById('hideText2');
+
+hideText_btn2.addEventListener('click', toggleText2);
+
+function toggleText2(){
+    hideText2.classList.toggle('show2');
+
+    if(hideText2.classList.contains('show2')){
+        hideText_btn2.innerHTML = 'Leer menos'
+    }
+    else {
+        hideText_btn2.innerHTML = 'Leer mas'
+    }
+}
+
+
+let hideText_btn3 = document.getElementById('hideText_btn3');
+
+let hideText3 = document.getElementById('hideText3');
+
+hideText_btn3.addEventListener('click', toggleText3);
+
+function toggleText3(){
+    hideText3.classList.toggle('show3');
+
+    if(hideText3.classList.contains('show3')){
+        hideText_btn3.innerHTML = 'Leer menos'
+    }
+    else {
+        hideText_btn3.innerHTML = 'Leer mas'
+    }
+}
